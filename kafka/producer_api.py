ab45f5097c3f2662cbf8037055eb01764ee13950
Paundra Pujo Darmawan <paundrapujodarmawan@gmail.com>
2026-04-28 19:27:10 +0700

    revisi

--- kafka/producer_api.py
+++ kafka/producer_api.py
@@ -3,15 +3,14 @@ import json
 import time
 from kafka import KafkaProducer
 
-API_TOKEN = "4bdec7a67818fbbb2dec75c14b32227f6fc9b96b"
 KAFKA_BROKER = 'localhost:9092'
 TOPIC_NAME = 'airquality-api'
 
 DAERAH_JATIM = [
-    "Bangkalan", "Banyuwangi", "Blitar", "Bojonegoro", "Bondowoso", "Gresik", 
-    "Jember", "Jombang", "Kediri", "Lamongan", "Lumajang", "Madiun", "Magetan", 
-    "Malang", "Mojokerto", "Nganjuk", "Ngawi", "Pacitan", "Pamekasan", "Pasuruan", 
-    "Ponorogo", "Probolinggo", "Sampang", "Sidoarjo", "Situbondo", "Sumenep", 
+    "Bangkalan", "Banyuwangi", "Blitar", "Bojonegoro", "Bondowoso", "Gresik",
+    "Jember", "Jombang", "Kediri", "Lamongan", "Lumajang", "Madiun", "Magetan",
+    "Malang", "Mojokerto", "Nganjuk", "Ngawi", "Pacitan", "Pamekasan", "Pasuruan",
+    "Ponorogo", "Probolinggo", "Sampang", "Sidoarjo", "Situbondo", "Sumenep",
     "Trenggalek", "Tuban", "Tulungagung", "Batu", "Surabaya"
 ]
 
@@ -23,13 +22,11 @@ def get_data(target, token):
 def fetch_and_produce():
     print("--- Memulai Ingestion API Jatim (Final Mode) ---")
     producer = KafkaProducer(bootstrap_servers=[KAFKA_BROKER], value_serializer=lambda v: json.dumps(v).encode('utf-8'))
-    
-    # Ambil data Surabaya sebagai fallback/referensi utama Jatim
-    ref_data = get_data("surabaya", API_TOKEN)
+
 
     for kota in DAERAH_JATIM:
         data = get_data(kota, API_TOKEN)
-        
+
         # Kalau kota tersebut gak ada sensor, pake data Surabaya (Reference)
         if not data:
             data = ref_data
@@ -48,7 +45,7 @@ def fetch_and_produce():
             }
             producer.send(TOPIC_NAME, value=payload)
             print(f"{status_text} {kota}: {payload['aqi']} AQI")
-        
+
         time.sleep(0.5)
     producer.flush()
     print("\nAPI Ingestion Selesai!")
